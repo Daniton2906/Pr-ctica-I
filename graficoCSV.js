6d75fdb941721d8eee0894c53d2d77e30a805081
Daniton2906 <ddiomedi2906@gmail.com>
2017-08-04 12:18:46 -0400

    First changes

--- graficoCSV.js
+++ graficoCSV.js
@@ -1,80 +1,100 @@
 // JavaScript source code
 
-var dataset = d3.csv("data/countCountryPerDomain.csv",
-    function (d, i, columns) { //row
-        for (var i = 0, n = columns.length; i < n; ++i)
-            d[columns[i]] = +d[columns[i]];
-        return d;
-    }, function (error, data) { //callback
-        if (error) throw error;
+//Width and height
+var w = 600,
+    h = 400,
+    barPadding = 10,
+    padding = 50
+    y_padding = 25;
 
-        var keys = data.columns.slice(0); //["numdomains", "countrycount"]
+var url_name = "data/countCountryPerDomain";
+var xScale = d3.scaleLinear()
+    .range([padding, w]);
+var yScale = d3.scaleLinear()
+    .range([h - y_padding, 2*y_padding]);
+var color = d3.scaleSequential(d3.interpolateBlues);
 
-        //console.log(keys);        
-        //console.log(data);
-
-
-        //Width and height
-        var w = 800;
-        var h = 200;
-        var barPadding = 25;  // <-- Nueva!
-
-        var maxValue = d3.max(data, function (d) { return d.numdomains; });
-        console.log(maxValue);
-
-        var yScale = d3.scaleLinear()
-            .domain([0, maxValue])
-            .range([h - barPadding, barPadding]);
-
-        var color = d3.scaleSequential(d3.interpolateBlues)
-            .domain([0, maxValue]);
-        console.log(color(0));
-        console.log(color(100));
-        console.log(color(maxValue));
-
-        //Crear un elemento SVG
-        var svg = d3.select("#featured-wrapper")
+//Crear un elemento SVG
+d3.select("#featured-wrapper")
             .append("svg")
             .attr("width", w)
             .attr("height", h)
             .attr("id", "graf2")
-            .style("background-color", "indianred");
+            .style("background-color", "navajowhite");
 
-        svg.selectAll("rect")
-            .data(data)
-            .enter()
-            .append("rect")
-            .attr("x", function (d, i) {
-                return i * (w / data.length);
-            })
-            .attr("y", function (d) {
-                return yScale(d.numdomains);  //Altura menos el dato
-            })
-            .attr("width", w / data.length - barPadding)
-            .attr("height", function (d) {
-                return h - yScale(d.numdomains);
-            })
-            .attr("fill", function (d) {
-                return color(d.numdomains);//"rgb(0, 0, " + (d * 10) + ")";
-            });
+function updateCSVBarChart(id, n, duration) {
 
-        svg.selectAll("text")
-            .data(data)
-            .enter()
-            .append("text")
-            .text(function (d) {
-                return d.numdomains;
-            })
-            .attr("x", function (d, i) {
-                return i * (w / data.length) + (w / data.length - barPadding) / 2;
-            })
-            .attr("y", function (d) {
-                return yScale(d.numdomains) + 15;
-            })
-            .attr("font-family", "sans-serif")
-            .attr("font-size", "11px")
-            .attr("fill", "black")
-            .attr("text-anchor", "middle");             
+    var dataset = d3.csv(url_name + n + ".csv",
+        function (d, i, columns) { //row
+            for (var i = 0, n = columns.length; i < n; ++i)
+                d[columns[i]] = +d[columns[i]];
+            return d;
+        }, function (error, data) { //callback
+            if (error) throw error;
 
-        
-    });
+            var svg = d3.selectAll(id);
+            
+            var maxValue = d3.max(data, function (d) { return d.numdomains; });
+
+            xScale.domain([0, data.length]);
+
+            yScale.domain([0, maxValue]);
+
+            color.domain([-maxValue, maxValue]);
+            
+            svg.selectAll("rect")
+                .data(data)
+                .enter()
+                .append("rect")
+                .transition()
+                .duration(duration)
+                .attr("x", function (d, i) {
+                    return xScale(i);
+                })
+                .attr("y", function (d) {
+                    return yScale(d.numdomains) - y_padding;  //Altura menos el dato
+                })
+                .attr("width", w / data.length - barPadding)
+                .attr("height", function (d) {
+                    return h - yScale(d.numdomains);
+                })
+                .attr("fill", function (d) {
+                    return color(0);//color(d.numdomains);
+                })
+                .attr("stroke-width", "1px")
+                .attr("stroke", "black");
+
+            svg.selectAll("text")
+                .data(data)
+                .enter()
+                .append("text")
+                .transition()
+                .duration(duration)
+                .text(function (d) {
+                    return d.numdomains;
+                })
+                .attr("x", function (d, i) {
+                    return xScale(i) + (w / data.length - barPadding) / 2;
+                })
+                .attr("y", function (d) {
+                    return yScale(d.numdomains) - 10;
+                })
+                .attr("font-family", "sans-serif")
+                .attr("font-size", "11px")
+                .attr("fill", "black")
+                .attr("text-anchor", "middle");  
+                
+            var yAxis = d3.axisLeft()
+            .scale(yScale)
+            .ticks(5);  //Número aproximado de marcadores
+
+            svg.selectAll(".axis").remove();
+            svg.append("g")
+                .attr("class", "axis")
+                .attr("transform", "translate(" + padding + ",0)")
+                .call(yAxis);
+
+        });
+}
+
+updateCSVBarChart("#graf2", "", 0);
